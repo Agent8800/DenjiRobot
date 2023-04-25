04fc441d6aaaf7f541c3bdb9196fb2117f9675e5
Agent8800 <90744224+Agent8800@users.noreply.github.com>
2023-04-25 16:08:51 +0530

    Update afk_sql.py

--- SaitamaRobot/modules/sql/afk_sql.py
+++ SaitamaRobot/modules/sql/afk_sql.py
@@ -4,12 +4,13 @@ from datetime import datetime
 
 from SaitamaRobot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, Integer, UnicodeText, DateTime
+from sqlalchemy.sql.sqltypes import BigInteger
 
 
 class AFK(BASE):
     __tablename__ = "afk_users"
 
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     is_afk = Column(Boolean)
     reason = Column(UnicodeText)
     time = Column(DateTime)
