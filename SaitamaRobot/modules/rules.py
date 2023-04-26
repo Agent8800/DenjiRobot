33da0120ce28fe6e472c5f03d0dea254d9efb1df
Agent8800 <90744224+Agent8800@users.noreply.github.com>
2023-04-26 12:58:16 +0530

    Update rules.py

--- SaitamaRobot/modules/rules.py
+++ SaitamaRobot/modules/rules.py
@@ -13,11 +13,11 @@ from telegram import (
     User,
 )
 from telegram.error import BadRequest
-from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
+from telegram.ext import CallbackContext, CommandHandler, Filters
 from telegram.utils.helpers import escape_markdown
 
 
-@run_async
+
 def get_rules(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
     send_rules(update, chat_id)
@@ -87,7 +87,7 @@ def send_rules(update, chat_id, from_pm=False):
         )
 
 
-@run_async
+
 @user_admin
 def set_rules(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
@@ -105,7 +105,7 @@ def set_rules(update: Update, context: CallbackContext):
         update.effective_message.reply_text("Successfully set rules for this group.")
 
 
-@run_async
+
 @user_admin
 def clear_rules(update: Update, context: CallbackContext):
     chat_id = update.effective_chat.id
@@ -141,9 +141,9 @@ __help__ = """
 
 __mod_name__ = "Rules"
 
-GET_RULES_HANDLER = CommandHandler("rules", get_rules, filters=Filters.group)
-SET_RULES_HANDLER = CommandHandler("setrules", set_rules, filters=Filters.group)
-RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, filters=Filters.group)
+GET_RULES_HANDLER = CommandHandler("rules", get_rules, Filters.chat_type.groups, run_async=True)
+SET_RULES_HANDLER = CommandHandler("setrules", set_rules, Filters.chat_type.groups, run_async=True)
+RESET_RULES_HANDLER = CommandHandler("clearrules", clear_rules, Filters.chat_type.groups, run_async=True)
 
 dispatcher.add_handler(GET_RULES_HANDLER)
 dispatcher.add_handler(SET_RULES_HANDLER)
