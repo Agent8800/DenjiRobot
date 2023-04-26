142725583014c1431941174ddf21a7a4b1fdc12b
Agent8800 <90744224+Agent8800@users.noreply.github.com>
2023-04-26 12:53:10 +0530

    Update speed_test.py

--- SaitamaRobot/modules/speed_test.py
+++ SaitamaRobot/modules/speed_test.py
@@ -3,7 +3,7 @@ from SaitamaRobot import DEV_USERS, dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
-from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
+from telegram.ext import CallbackContext, CallbackQueryHandler
 
 
 def convert(speed):
@@ -11,7 +11,6 @@ def convert(speed):
 
 
 @dev_plus
-@run_async
 def speedtestxyz(update: Update, context: CallbackContext):
     buttons = [
         [
@@ -24,7 +23,7 @@ def speedtestxyz(update: Update, context: CallbackContext):
     )
 
 
-@run_async
+
 def speedtestxyz_callback(update: Update, context: CallbackContext):
     query = update.callback_query
 
@@ -51,10 +50,10 @@ def speedtestxyz_callback(update: Update, context: CallbackContext):
         query.answer("You are required to join Heroes Association to use this command.")
 
 
-SPEED_TEST_HANDLER = DisableAbleCommandHandler("speedtest", speedtestxyz)
+SPEED_TEST_HANDLER = DisableAbleCommandHandler("speedtest", speedtestxyz, run_async=True)
 SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
-    speedtestxyz_callback, pattern="speedtest_.*",
-)
+    speedtestxyz_callback, pattern="speedtest_.*", run_async=True)
+
 
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
