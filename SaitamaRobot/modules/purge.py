e478d7359f4833e5f6dcfa54af9f136a53fc8133
Agent8800 <90744224+Agent8800@users.noreply.github.com>
2023-04-26 13:25:51 +0530

    Update purge.py

--- SaitamaRobot/modules/purge.py
+++ SaitamaRobot/modules/purge.py
@@ -77,8 +77,8 @@ __help__ = """
  - /purge <integer X>: deletes the replied message, and X messages following it if replied to a message.
 """
 
-PURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]purge$")
-DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$")
+PURGE_HANDLER = purge_messages, events.NewMessage(pattern="^[!/]purge$", run_async=True)
+DEL_HANDLER = delete_messages, events.NewMessage(pattern="^[!/]del$", run_async=True)
 
 telethn.add_event_handler(*PURGE_HANDLER)
 telethn.add_event_handler(*DEL_HANDLER)
