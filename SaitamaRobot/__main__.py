a910d6410120bc99e60ce3cc13f9ddd2d46ff6bd
Ishant <90744224+Agent8800@users.noreply.github.com>
2023-04-27 14:16:58 +0530

    Denji

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -671,7 +671,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I'm online now! 👩‍💼")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Awake Now!")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
