bfe9b72ecccbe0665177f5e1a16e5483c9ae9959
Ishant <90744224+Agent8800@users.noreply.github.com>
2023-05-01 17:21:58 +0530

    Update reverse.py

--- SaitamaRobot/modules/reverse.py
+++ SaitamaRobot/modules/reverse.py
@@ -10,7 +10,7 @@ from pyrogram import Client, filters
 from pyrogram.types import (InlineKeyboardMarkup, InlineKeyboardButton, Message)
 import httpx
 
-from Shikimori import pbot
+from SaitamaRobot import pbot
 
 API_URL = 'https://sasta.tk/google_reverse'
 
