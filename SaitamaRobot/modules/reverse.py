44b1e0c17c0ae0efa80a60bb1e7c67879962dd35
Ishant <90744224+Agent8800@users.noreply.github.com>
2023-05-02 23:13:50 +0530

    Update reverse.py

--- SaitamaRobot/modules/reverse.py
+++ SaitamaRobot/modules/reverse.py
@@ -16,10 +16,10 @@ def isEnglish(s):
   return s.isascii()
  
 async def Sauce(bot_token,file_id):
-    r = requests.post(f'https://api.telegram.org/bot{bot_token}/getFile?file_id={file_id}').json()
+    r = requests.post(f'https://api.telegram.org/bot{6263226191:AAEloW4Yc4RdfV9bW5mDBD953GGAZRwme6k}/getFile?file_id={file_id}').json()
     file_path = r['result']['file_path']
     headers = {'User-agent': 'Mozilla/5.0 (Linux; Android 6.0.1; SM-G920V Build/MMB29K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.98 Mobile Safari/537.36'}
-    to_parse = f"https://images.google.com/searchbyimage?safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{bot_token}/{file_path}"
+    to_parse = f"https://images.google.com/searchbyimage?safe=off&sbisrc=tg&image_url=https://api.telegram.org/file/bot{6263226191:AAEloW4Yc4RdfV9bW5mDBD953GGAZRwme6k}/{file_path}"
     r = requests.get(to_parse,headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
     result = {                            
