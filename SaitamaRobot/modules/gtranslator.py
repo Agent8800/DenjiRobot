b6b68a2c8d37c7e9522061ad7805e32380a46e40
Ishant <90744224+Agent8800@users.noreply.github.com>
2023-05-06 20:30:53 +0530

    Update gtranslator.py

--- SaitamaRobot/modules/gtranslator.py
+++ SaitamaRobot/modules/gtranslator.py
@@ -1,106 +1,68 @@
-from emoji import UNICODE_EMOJI
-from google_trans_new import LANGUAGES, google_translator
-from telegram import ParseMode, Update
+from telegram import Update, ParseMode
 from telegram.ext import CallbackContext
-
+from gpytranslate import SyncTranslator
 from SaitamaRobot import dispatcher
 from SaitamaRobot.modules.disable import DisableAbleCommandHandler
 
+trans = SyncTranslator()
 
 
-def totranslate(update: Update, context: CallbackContext):
+def totranslate(update: Update, context: CallbackContext) -> None:
     message = update.effective_message
-    problem_lang_code = []
-    for key in LANGUAGES:
-        if "-" in key:
-            problem_lang_code.append(key)
-
-    try:
-        if message.reply_to_message:
-            args = update.effective_message.text.split(None, 1)
-            if message.reply_to_message.text:
-                text = message.reply_to_message.text
-            elif message.reply_to_message.caption:
-                text = message.reply_to_message.caption
-
-            try:
-                source_lang = args[1].split(None, 1)[0]
-            except (IndexError, AttributeError):
-                source_lang = "en"
-
-        else:
-            args = update.effective_message.text.split(None, 2)
-            text = args[2]
-            source_lang = args[1]
-
-        if source_lang.count("-") == 2:
-            for lang in problem_lang_code:
-                if lang in source_lang:
-                    if source_lang.startswith(lang):
-                        dest_lang = source_lang.rsplit("-", 1)[1]
-                        source_lang = source_lang.rsplit("-", 1)[0]
-                    else:
-                        dest_lang = source_lang.split("-", 1)[1]
-                        source_lang = source_lang.split("-", 1)[0]
-        elif source_lang.count("-") == 1:
-            for lang in problem_lang_code:
-                if lang in source_lang:
-                    dest_lang = source_lang
-                    source_lang = None
-                    break
-            if dest_lang is None:
-                dest_lang = source_lang.split("-")[1]
-                source_lang = source_lang.split("-")[0]
-        else:
-            dest_lang = source_lang
-            source_lang = None
-
-        exclude_list = UNICODE_EMOJI.keys()
-        for emoji in exclude_list:
-            if emoji in text:
-                text = text.replace(emoji, "")
-
-        trl = google_translator()
-        if source_lang is None:
-            detection = trl.detect(text)
-            trans_str = trl.translate(text, lang_tgt=dest_lang)
-            return message.reply_text(
-                f"Translated from `{detection[0]}` to `{dest_lang}`:\n`{trans_str}`",
-                parse_mode=ParseMode.MARKDOWN,
-            )
-        else:
-            trans_str = trl.translate(text, lang_tgt=dest_lang, lang_src=source_lang)
-            message.reply_text(
-                f"Translated from `{source_lang}` to `{dest_lang}`:\n`{trans_str}`",
-                parse_mode=ParseMode.MARKDOWN,
-            )
-
-    except IndexError:
-        update.effective_message.reply_text(
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        message.reply_text(
             "Reply to messages or write messages from other languages ​​for translating into the intended language\n\n"
-            "Example: `/tr en-ml` to translate from English to Malayalam\n"
-            "Or use: `/tr ml` for automatic detection and translating it into Malayalam.\n"
-            "See [List of Language Codes](t.me/OnePunchSupport/12823) for a list of language codes.",
+            "Example: `/tr en-hi` to translate from English to Hindi\n"
+            "Or use: `/tr en` for automatic detection and translating it into english.\n"
+            "Click here to see [List of available Language Codes](https://telegra.ph/X-E-L-C-I-U-S--%E5%89%A3-07-31).",
             parse_mode="markdown",
             disable_web_page_preview=True,
         )
-    except ValueError:
-        update.effective_message.reply_text("The intended language is not found!")
-    else:
         return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = trans.detect(to_translate)
+        dest = "en"
+    translation = trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = (
+        f"<b>ᴛʀᴀɴsʟᴀᴛᴇᴅ ғʀᴏᴍ {source} ᴛᴏ {dest}</b> :\n"
+        f"<code>{translation.text}</code>"
+    )
+
+    message.reply_text(reply, parse_mode=ParseMode.HTML)
 
 
 __help__ = """
-• `/tr` or `/tl` (language code) as reply to a long message
-*Example:*
-  `/tr en`*:* translates something to english
-  `/tr hi-en`*:* translates hindi to english
+ ❍ /tr or /tl (language code) as reply to a long message
+*Example:* 
+ ❍ /tr en*:* translates something to english
+ ❍ /tr hi-en*:* translates hindi to english
+
+*Language Codes*
+`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
+id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
 """
+__mod_name__ = "Tʀᴀɴsʟᴀᴛᴏʀ"
 
 TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate, run_async=True)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
 
-__mod_name__ = "Translator"
 __command_list__ = ["tr", "tl"]
 __handlers__ = [TRANSLATE_HANDLER]
